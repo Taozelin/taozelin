7b2558c308510cf035a38a622136cbb05196903f
Taozelin <547628495@qq.com>
2017-06-05 15:54:21 +0800

    添加注释

--- miaov/Javascript基础/9-定时器管理、函数封装/js/miaov.js
+++ miaov/Javascript基础/9-定时器管理、函数封装/js/miaov.js
@@ -1,22 +1,22 @@
 
-function doMove ( obj, attr, dir, target, endFn ) {
+function doMove ( obj, attr, dir, target, endFn ) {  //要运动的元素 要运动的属性 每次运动的方向 目标点 运行完后触发的函数
 	
-	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
+	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;   //如果目标点大于当前位置 方向为正 反之亦然
 	
-	clearInterval( obj.timer );
+	clearInterval( obj.timer );    //防止点击多次 导致速度越来越快
 	
 	obj.timer = setInterval(function () {
 		
-		var speed = parseInt(getStyle( obj, attr )) + dir;			// 步长
+		var speed = parseInt(getStyle( obj, attr )) + dir;			// 获取当前位置 不断移动
 		
 		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
-			speed = target;
+			speed = target;                            //如果超过目标点，就提前拉回来
 		}
 		
-		obj.style[attr] = speed + 'px';
-		
+		obj.style[attr] = speed + 'px';    //不断变化的属性，形成动画
+		 
 		if ( speed == target ) {
-			clearInterval( obj.timer );
+			clearInterval( obj.timer );     //到达目标点停止定时器
 			
 			/*
 			if ( endFn ) {
@@ -30,7 +30,7 @@ function doMove ( obj, attr, dir, target, endFn ) {
 	}, 30);
 }
 /*
-function opacity(obj, num, target, endFn) {
+function opacity(obj, num, target, endFn) {    //原理和上面的差不多
 	
 		num = getStyle(obj, 'opacity')*100 < target ? num : -num;
 		
